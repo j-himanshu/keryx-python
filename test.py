17cf0c5f37362674007840bc6b97ea8cdb617bbf
j-himanshu <hs.9731391859@outlook.com>
2017-09-12 19:06:20 +0530

    TEST GIT  CoMMIT[2]

---
+++ test.py
@@ -0,0 +1,12 @@
+import wave
+
+def magic(audioFileName):
+    audioFile = wave.open(audioFileName, "r")
+    props = audioFile.getparams()
+    oldFrames = audioFile.readframes(props[3])
+    newFrames = []
+    for frame in oldFrames:
+        byte = frame[0]
+        char = chr(byte)
+        asci = ord(char)
+
